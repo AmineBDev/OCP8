6c4e5303d2cb0253429f1ae7e6ef7da566a3b0a5
Amine Bensaoucha <bensaoucha84@gmail.com>
2024-08-14 00:54:20 +0200

    header et presentation mis en place

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h2>Amine.dev</h2>
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>À propos</li>
+                    <li>Projets</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+import Presentation from '../components/Presentation';
+
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Presentation />
+        </>
+    );
+};
+
+export default Home;
