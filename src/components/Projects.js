43ff58444f4d582a52cea085105fe990d186703a
Amine Bensaoucha <bensaoucha84@gmail.com>
2024-09-09 19:35:37 +0200

    section projets terminée

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 const Projects = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
     useEffect(() => {
 
@@ -20,12 +21,20 @@ const Projects = () => {
 
     }, [])
 
+    const handleCloseModal = (e) => {
+        if (e.target.classList.contains('modal-main')) {
+            setIsModalOpen(false);
+        }
+    };
+
     return (
         <section className='projects'>
             <div className="projects-container">
                 <h3>MES PROJETS</h3>
                 <h2>Chaque projet est une création unique en développement 🧩</h2>
-                {loading ? <i class="fa-solid fa-spinner fa-spin-pulse"></i> :
+                {loading ? <div className="loading-project">
+                    <i class="fa-solid fa-spinner fa-spin-pulse"></i>
+                </div> :
                     data.slice(0, 3)
                         .map((projet) => (
                             <div key={projet.title} className="project">
@@ -48,8 +57,47 @@ const Projects = () => {
                                 </div>
                             </div>
                         ))}
-                <button>Voir plus</button>
+                {!loading && (
+                    <div className="btn-project">
+                        <button onClick={() => setIsModalOpen(true)}>Voir plus</button>
+                    </div>
+                )}
             </div>
+            {/* Modale */}
+            {isModalOpen && (
+                <div className="modal-main" onClick={handleCloseModal}>
+                    <div className="modal-main-container">
+                        <div className="header-modal">
+                            <i onClick={() => setIsModalOpen(false)} className="fa-solid fa-xmark"></i>
+                        </div>
+                        <div className="projects-modal">
+                            {data
+                                .slice(3, data.length)
+                                .map((projet) => (
+                                    <div key={projet.title} className="project-modal">
+                                        <div className="img-project-modal">
+                                            <img src={projet.image} alt={"Photo du projet " + projet.title} />
+                                        </div>
+                                        <div className="description-project-modal">
+                                            <h3>{projet.title}</h3>
+                                            <p className='descr-project-modal'>Contexte : {projet.description}</p>
+                                            <p>{projet.type}</p>
+                                            <ul>
+                                                {projet.tech.map((tech, techIndex) => (
+                                                    <li key={techIndex}>{tech}</li>
+                                                ))}
+                                            </ul>
+                                            <ul>
+                                                <li><a href={projet.git} target="_blank" rel="noopener noreferrer">Code <span><i class="fa-brands fa-github"></i></span></a></li>
+                                                <li><a href={projet.link} target="_blank" rel="noopener noreferrer">Live Demo <span><i class="fa-solid fa-arrow-up-right-from-square"></i></span></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                </div>
+            )}
         </section>
     );
 };
